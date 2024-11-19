85cbbb203bb6e95eb061e659d196e32d6ed609e0
Luis Flor Chacón <luisflorchacon7@gmail.com>
2024-11-19 15:50:32 +0000

    Poker Card randomize with inputs to change width and height plus the addeventlisteners for button and for inputs

--- src/app.js
+++ src/app.js
@@ -5,6 +5,32 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const inputWidth = document.getElementById("inputWidth");
+const inputHeight = document.getElementById("inputHeight");
+const button = document.getElementById("button");
+
+function newWidth(size) {
+  carta.style.width = `${size}px`;
+}
+function newHeight(size) {
+  carta.style.height = `${size}px`;
+}
+button.addEventListener("click", e => {
+  carta.style.width = "270px";
+  carta.style.height = "400px";
+});
+inputWidth.addEventListener("blur", e => {
+  if (e.target.value) {
+    newWidth(e.target.value);
+    e.target.value = "";
+  }
+});
+inputHeight.addEventListener("blur", e => {
+  if (e.target.value) {
+    newHeight(e.target.value);
+    e.target.value = "";
+  }
+});
 const simbolos = ["♦", "♥", "♠", "♣"];
 const numeros = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
 function randomizarNumeros() {
@@ -19,6 +45,33 @@ function randomizarSimbolos() {
 function colorSimbolos(simbolo) {
   return simbolo === "♦" || simbolo === "♥" ? "red" : "black";
 }
+
+let carta = document.getElementById("carta");
+carta.addEventListener("click", function() {
+  randomizarNumeros();
+  let simboloRandom = randomizarSimbolos();
+  let colorRandom = colorSimbolos(simboloRandom);
+  let simboloArriba = document.getElementById("simboloArriba");
+  let simboloAbajo = document.getElementById("simboloAbajo");
+  simboloArriba.innerHTML = simboloRandom;
+  simboloArriba.style.color = colorRandom;
+  simboloAbajo.innerHTML = simboloRandom;
+  simboloAbajo.style.color = colorRandom;
+});
+
+function actualizarCarta() {
+  randomizarNumeros();
+  let simboloRandom = randomizarSimbolos();
+  let colorRandom = colorSimbolos(simboloRandom);
+  let simboloArriba = document.getElementById("simboloArriba");
+  let simboloAbajo = document.getElementById("simboloAbajo");
+  simboloArriba.innerHTML = simboloRandom;
+  simboloArriba.style.color = colorRandom;
+  simboloAbajo.innerHTML = simboloRandom;
+  simboloAbajo.style.color = colorRandom;
+}
+
+setInterval(actualizarCarta, 10000);
 
 window.onload = function() {
   //write your code here
